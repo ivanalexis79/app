b77ae92effb71f9d87b7ce0ee665d3858db2c2f3
ivanalexis79 <ivanalexis.gonzalez@gmail.com>
2025-05-18 22:34:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ function convert() {
 
 // Registrar Service Worker (para PWA)
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log("Service Worker registrado con éxito", reg))
+    .catch(err => console.error("Error al registrar el Service Worker", err));
 }
