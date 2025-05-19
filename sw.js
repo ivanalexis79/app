39b0ee572ef70548f17a6206f2d733fb830518cd
ivanalexis79 <ivanalexis.gonzalez@gmail.com>
2025-05-18 23:03:06 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('app-cache').then(cache => {
-            return cache.add('/index.html')
+            return cache.add('/app/index.html')
             .catch(err => console.error("Error al agregar index.html al caché:", err));
         })
     );
